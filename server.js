05fb8a8cb0ea59fd546d45d801c023e62cefe9f2
Xiaoya Zou <84748829+xiaoyazz@users.noreply.github.com>
2025-04-08 10:00:16 -0400

    Trigger workflow new

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-    res.send('Hello, Dockerized Node.js App V2!!!');
+    res.send('Hello, Dockerized Node.js App V2!!! demo');
 });
 app.listen(PORT, () => {
     console.log(`App running on http://localhost:${PORT}`);
